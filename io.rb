7f1a38e6fe6cc73dc06f52d10965246be288a3b2
jaderzk <jader.rojas@gmail.com>
2018-04-01 12:24:39 -0500

    Creando un método para sumar que recive dos parámetros

--- io.rb
+++ io.rb
@@ -1,21 +1,20 @@
 #Entrada y salida de datos
 
-print "Hola \nIngresa tu Nombre"
+print "Hola \nIngresa tu Nombre: "
 nombre=gets.chomp.to_s
-puts "Bienvenido a Ruby #{nombre}"
+puts "Bienvenido a Ruby Básico #{nombre}"
 
 
-print "Sumando dos números \nIngrese primer número: "
-numero=gets.chomp.to_i
+print "Sumando dos números \n\nIngrese primer número: "
+numero_uno=gets.chomp.to_i
 
 print "Ingrese segundo número: "
 numero_dos=gets.chomp.to_i
-
-puts "El resultado de la suma es #{numero+numero_dos}"
 
 
 def sumar_dos_numeros (numero_uno, numero_dos)
   numero_uno+numero_dos
 end	
 
-puts sumar_dos_numeros(3,2)
+puts "El resultado de la suma es #{sumar_dos_numeros(numero_uno,numero_dos)}"
+
