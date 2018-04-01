296cb2f208dec4c87058c4a09154a0f3e6b45df5
jaderzk <jader.rojas@gmail.com>
2018-04-01 12:04:58 -0500

    Sumando dos números enteros en Ruby

--- io.rb
+++ io.rb
@@ -3,3 +3,12 @@
 print "Hola \nIngresa tu Nombre"
 nombre=gets.chomp.to_s
 puts "Bienvenido a Ruby #{nombre}"
+
+
+print "Sumando dos números \nIngrese primer número: "
+numero=gets.chomp.to_i
+
+print "Ingrese segundo número: "
+numero_dos=gets.chomp.to_i
+
+puts "El resultado de la suma es #{numero+numero_dos}"
